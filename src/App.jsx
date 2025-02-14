f2196cd6c38caf2ee6b8563cab12dedbc897d53c
Zeta80 <axxel1700@gmail.com>
2025-02-14 19:04:06 +0100

    card map array

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
 import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  const cities =[ 
+        {
+          id:0,
+          isVisited:true,
+          title:"London" ,
+          imgUrl:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.",
+        },
+        {
+          id:1,
+          isVisited:false,
+          title:"Paris" ,
+          imgUrl:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.",
+        },
+        {
+          id:2,
+          isVisited:true,
+          title:"New York" ,
+          imgUrl:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.",
+        },
+        {
+          id:3,
+          isVisited:false,
+          title:"Tokyo" ,
+          imgUrl:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.",
+        },
+       
+]
 
   return (
     <>
 
       <div className='grid grid-cols-4 gap-10'>
-        <Card 
-          isVisited={true}
-          title="Tokyo" 
-          imgUrl="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.
-        </Card>
-        <Card 
-          isVisited={false}
-          title="Tokyo" 
-          imgUrl="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.
-        </Card>
-        <Card 
-          isVisited={true}
-          title="Tokyo" 
-          imgUrl="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.
-        </Card>
-        <Card 
-          isVisited={false}
-          title="Tokyo" 
-          imgUrl="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, incidunt.
-        </Card>
+
+        {cities.map((city) => (
+          <Card key={city.id} title={city.title} imgUrl={city.imgUrl} isVisited={city.isVisited}>
+            {city.text}
+          </Card>
+        ))}
+
       </div>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
