4b4be45cbba61f26a4d5dadcc652000e5c0155d0
Zeta80 <axxel1700@gmail.com>
2025-02-15 03:37:33 +0100

    fix

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 
 function CardForm({addCity}) {
-    
-        //   imgUrl:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
-         
+             
     const [formData, setFormData] = useState(
         {
             title: "",
